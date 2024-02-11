2f31c050551a36804afe475cbb2397d8cac10068
Sana Parveen <misana348@gmail.com>
2024-02-11 16:51:11 +0530

    Home page folder

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
-
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from "./components/Home";
 function App() {
   
 
   return (
     <>
-      <h1 className='bg-green-600 p-4' >React router</h1>
+      <Header/>
+      <Home/>
+
+      <Footer/>
     </>
   )
 }
