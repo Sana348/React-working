e41199503cfc9d5654593801121e51797b73257b
Sana Parveen <misana348@gmail.com>
2024-02-12 17:24:16 +0530

    User login with context

--- context/src/App.jsx
+++ context/src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
-import UserContext from './context/UserContext'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
- 
+  
 
   return (
-    <UserContext.Provider><h1>React with react</h1></UserContext.Provider>
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
--- context/src/components/Profile.jsx
+++ context/src/components/Profile.jsx
@@ -1,11 +1,12 @@
-import React, {useContext} from 'react'
+import  {useContext} from 'react'
 import UserContext from '../context/UserContext'
 
 function Profile() {
-const {user} = useContext(UserContext)
-      
-if (!user) return <div>Please log in profile</div> 
-return <div>Welcome {user.username}</div>
+    const {user} = useContext(UserContext)
+    
+    if (!user) return <div>please login</div>
+
+    return <div>Welcome {user.username}</div>
 }
 
 export default Profile
